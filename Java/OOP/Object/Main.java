4f690ef17be649baacae948eeed800ef6efb0f0d
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-07-22 16:23:28 +0530

    Create java objects

---
+++ Java/OOP/Object/Main.java
@@ -0,0 +1,12 @@
+// Creating multiple objects
+
+package main;
+
+public class Main {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Main object = new Main();
+        System.out.println(object.x);
+    }
+}
---
+++ Java/OOP/Object/MultipleObj.java
@@ -0,0 +1,12 @@
+// Creating multiple objects
+public class MultipleObj {
+    int x = 5;
+
+    public static void main(String[] args) {
+        MultipleObj object1 = new MultipleObj();
+        MultipleObj object2 = new MultipleObj();
+
+        System.out.println(object1.x);
+        System.out.println(object2.x);
+    }
+}
