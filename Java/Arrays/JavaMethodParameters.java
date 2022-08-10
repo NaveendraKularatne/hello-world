83228fdf18122f41360e73b6b20801f6d7fc513b
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-08-11 00:14:49 +0530

    Java methods

---
+++ Java/Arrays/JavaMethodParameters.java
@@ -0,0 +1,9 @@
+public class JavaMethodParameters{
+	public static void main(String[] args){
+		namePrint("Naveendra");
+	}
+
+	static void namePrint(String name){
+		System.out.println("Hello there " + name);
+	}
+}
---
+++ Java/Arrays/StaticMethodTest.java
@@ -0,0 +1,9 @@
+class  StaticMethodTest{
+	public static void main(String[] args){
+		printThis();
+	}
+
+	static void printThis(){
+		System.out.println("Print this method.");
+	}
+}
