4ad4c7bccecfbe2866faddb63dd9654bbd790cd1
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-09-08 03:39:39 +0530

    Test polymorphism

---
+++ Java/OOP/Polymorphism/Method Override/Animal.java
@@ -0,0 +1,18 @@
+public class Animal{
+	void animalSound(){
+		System.out.println("Animal make sounds");
+	} 
+}
+
+class Pig extends Animal{
+	void animalSound(){
+		System.out.println("Pig says: oink");
+	}
+}
+
+class Main{
+	public static void main(String[] args){
+		Animal newAnimal = new Animal();
+		newAnimal.animalSound();
+	}
+}
---
+++ Java/OOP/Polymorphism/TestOverload.java
@@ -0,0 +1,16 @@
+ class Adder{
+	 static int add(int a, int b){
+		return a+b;
+	}
+
+	 static int add(int a, int b, int c){
+		return a+b+c;
+	}
+}
+class TestOverload{
+	public static void main(String[] args){
+		//Adder addThis = new Adder();
+		System.out.println(Adder.add(11,11));
+		System.out.println(Adder.add(11,11,11));
+	}
+}
